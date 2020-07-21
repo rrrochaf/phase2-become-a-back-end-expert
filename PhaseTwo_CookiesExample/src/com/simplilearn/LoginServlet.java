5fb65e041f94923f1888fd48b69175dbf7c9161d
rrrochaf <ricardorrf@gmail.com>
2020-07-21 20:46:20 -0300

    Servlet Fundamentals - Cookies

---
+++ PhaseTwo_CookiesExample/src/com/simplilearn/LoginServlet.java
@@ -0,0 +1,36 @@
+package com.simplilearn;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public LoginServlet() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter writer = response.getWriter();
+		String name = request.getParameter("username");
+		//String pass = request.getParameter("userpass");
+	
+		// Create Cookies 
+		Cookie c1 = new Cookie("username",name);
+		
+		// Add the cookies to response 
+		response.addCookie(c1);
+		writer.println("<a href='TestServlet'>View Details</a>");
+	}
+
+}
---
+++ PhaseTwo_CookiesExample/src/com/simplilearn/TestServlet.java
@@ -0,0 +1,30 @@
+package com.simplilearn;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public TestServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Cookie[] cookies = request.getCookies();
+
+		PrintWriter writer = response.getWriter();
+
+		writer.println("Cookie Value : " + cookies[0].getValue());
+
+	}
+
+}
